c63a75a33a71e2e2e72e9a0f49fb617031236b03
Kim, dong su <hucke@hanmail.net>
2023-06-01 15:40:24 +0900

    [feature] p37

--- PrimeFactors/prime_factor.cpp
+++ PrimeFactors/prime_factor.cpp
@@ -9,6 +9,10 @@ public:
     vector<int> of(int i)
     {
         vector<int> result = {};
+        if ( i == 2 )
+        {
+            result.push_back(2);
+        }
         return result;
     }
 };
