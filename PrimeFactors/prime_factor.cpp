eecf43d28739abdca87c4a2d2978a598c5115aa4
Kim, dong su <hucke@hanmail.net>
2023-06-01 15:45:14 +0900

    [feature] p40

--- PrimeFactors/prime_factor.cpp
+++ PrimeFactors/prime_factor.cpp
@@ -6,12 +6,16 @@ using namespace std;
 class PrimeFactor
 {
 public:
-    vector<int> of(int i)
+    vector<int> of(int number)
     {
         vector<int> result = {};
-        if ( i == 2 )
+        if ( number == 2 )
         {
             result.push_back(2);
+        }
+        if (number == 3)
+        {
+            result.push_back(3);
         }
         return result;
     }
